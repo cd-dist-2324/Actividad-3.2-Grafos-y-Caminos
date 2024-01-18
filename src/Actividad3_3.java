deaa6b874d8a0a43289dc0f9dd95a104ae85ba7c
add-code-2223 <add.code.2223@gmail.com>
2024-01-18 20:23:18 +0100

    add description

--- src/Actividad3_3.java
+++ src/Actividad3_3.java
@@ -3,6 +3,13 @@
 public class Actividad3_3 {
 
     // apartado 1 division
+    /**
+     * Calcula el resultado de dividir dividendo entre divisor
+     * @param dividendo
+     * @param divisor
+     * @return resultado de la división
+     * @throws Exception Se lanza una excepción cuando el divisor es cero
+     */
     public float calcularDivision(float dividendo, float divisor) throws Exception {
 
         if (divisor == 0) {
@@ -18,6 +25,12 @@ public class Actividad3_3 {
     }
 
     // apartado 2
+    /**
+     * Calcula el factorial de un número n
+     * @param n
+     * @return el factorial de n
+     * @throws Exception Se lanza una excepción si n<0
+     */
     public float factorial(byte n) throws Exception {
 
         if (n < 0) {
@@ -39,6 +52,13 @@ public class Actividad3_3 {
     }
 
     // apartado 3
+    /**
+     * Recibe un carácter c y una matriz de caracteres v de como máximo 10 elementos ordenados en orden ascendente.
+     * La búsqueda es dicotómica, es decir, la  búsqueda se realiza calculando el índice mitad del segmento (en principio todo el array) y comparando c con el elemento almacenado en ese índice; si coincide, la búsqueda finaliza y se encuentra el carácter en la matriz; si c es menor, el siguiente segmento será la primera mitad del actual; si c es mayor, el siguiente segmento será la segunda mitad del actual. Si este proceso termina con un segmento nulo y no se encuentra el carácter, no existe y se devolverá false
+     * @param c carácter c
+     * @param v  matriz de caracteres v de como máximo 10 elementos ordenados en orden ascendente
+     * @return   Devuelve el valor booleano verdadero o falso dependiendo de si el carácter se encuentra en la matriz o no.
+     */
     public boolean busca(char c, char[] v) {
 
         int a, z, m;
@@ -84,6 +104,11 @@ public class Actividad3_3 {
     }
 
     // apartado 4
+    /**
+     * Recibe una cadena de caracteres y devuelve una cadena con el acrónimo correspondiente. 
+     * @param cadena de entrada
+     * @return El acrónimo está formado por el primer carácter de cada palabra seguido de un punto cuando el carácter es diferente del espacio en blanco.
+     */
     public String obtenerAcronimo(String cadena) {
 
         String resultado = "";
@@ -130,7 +155,7 @@ public class Actividad3_3 {
         byte entrada = 3;
         float resultado_factorial = app.factorial(entrada);
 
-        boolean encontrado = app.busca('p', "Busco una p".toCharArray());
+        boolean encontrado = app.busca('p', "acfhiprt".toCharArray());
 
         String acronimo = app.obtenerAcronimo("Formación Profesional");
 
